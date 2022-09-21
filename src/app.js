c716e83ba836d9942c11ae191052bd9a5d61b9b6
Aleisson <caioaleisson@gmail.com>
2022-09-21 14:21:54 -0300

    update app and create statusCode.enum

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import { json } from 'express';
+import { STATUS_CODE } from './enums/statusCode.enum.js';
 import cors from 'cors';
 
 dotenv.config();
@@ -9,6 +10,9 @@ const app = express();
 
 app.use(cors());
 app.use(json());
+//Teste de respota no servidor
+app.get('/', (req, res) => { res.status(STATUS_CODE.OK).send("<h1>Drivencracy</h1>") })
+
 
 
 app.listen(process.env.PORT, () => {
