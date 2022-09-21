dfcc281e019e2823971d952520f0870b1241f499
Aleisson <caioaleisson@gmail.com>
2022-09-21 15:39:17 -0300

    fix choice routes

--- src/routes/choice.Routes.js
+++ src/routes/choice.Routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/choice', (req, res) => { });
 
-router.get('/poll/:id/result', (req, res) => { });
+router.get('/choice/:id/vote', (req, res) => { });
 
 
 export default router;
